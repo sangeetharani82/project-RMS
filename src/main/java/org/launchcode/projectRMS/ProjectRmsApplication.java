173e685710bf52cc5d1b06fa3ffaec66a35046a0
Sangeetha Rani <sangeetha.sabarinath@gmail.com>
2019-01-27 19:35:51 -0600

    initial commit

---
+++ src/main/java/org/launchcode/projectRMS/ProjectRmsApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.projectRMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRmsApplication.class, args);
+	}
+
+}
+
